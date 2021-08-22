4358267934cc9ea1cae62cb12cfacf79fdcddacc
Yusuf Elley <yelley2001@gmail.com>
2021-08-22 01:02:06 -0400

    add interval functionality

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -9,19 +9,19 @@ export const Timer = () => {
 
   const [quickButtons, setQuickButtons] = useState({
     set1: {
-      numSets: "2 SETS",
+      numSets: "2",
       active: "false",
     },
     set2: {
-      numSets: "3 SETS",
+      numSets: "3",
       active: "false",
     },
     set3: {
-      numSets: "5 SETS",
+      numSets: "5",
       active: "false",
     },
     on1: {
-      duration: "20",
+      duration: "5",
       active: "false",
     },
     on2: {
@@ -33,7 +33,7 @@ export const Timer = () => {
       active: "false",
     },
     off1: {
-      duration: "20",
+      duration: "3",
       active: "false",
     },
     off2: {
@@ -46,14 +46,37 @@ export const Timer = () => {
     },
   });
 
+  const [timerInstructions, setTimerInstructions] = useState({
+    numSets: "0",
+    timeOn: "0",
+    timeOff: "0",
+    currentTimer: "TIME_ON",
+  });
+
   useEffect(() => {
     let intervalId;
 
-    if (counter < 0) {
+    if (counter < 0 && timerInstructions.currentTimer === "TIME_ON") {
+      setTimerInstructions({ ...timerInstructions, currentTimer: "TIME_OFF" });
+      setCounter(timerInstructions.timeOff);
+    }
+
+    if (counter < 0 && timerInstructions.currentTimer === "TIME_OFF") {
+      setTimerInstructions({
+        ...timerInstructions,
+        numSets: timerInstructions.numSets - 1,
+        currentTimer: "TIME_ON",
+      });
+      setCounter(timerInstructions.timeOn);
+    }
+
+    if (timerInstructions.numSets <= 0) {
       setIsActive(false);
+      console.log("CHECK IS ACTIVE: " + isActive);
     }
 
     if (isActive && counter >= 0) {
+      console.log("IS ACTIVE AND COUNTER >= 0: " + counter);
       intervalId = setInterval(() => {
         const secondCounter = counter % 60;
         const minuteCounter = Math.floor(counter / 60);
@@ -79,6 +102,7 @@ export const Timer = () => {
 
   return (
     <div className="content-wrapper">
+      {/* TIMER TEXT */}
       <div className="timer-display">
         <div className="timer text">
           <span className="minute">{minute}</span>
@@ -89,29 +113,124 @@ export const Timer = () => {
       </div>
 
       <div className="quick-timer-wrapper">
+        {/* SET BUTTONS */}
         <div className="quick-timer-btn">
-          <button className="btn" onClick={() => {}}>
+          <button
+            className="btn"
+            onClick={() => {
+              setTimerInstructions({
+                ...timerInstructions,
+                numSets: quickButtons.set1.numSets,
+              });
+            }}
+          >
             {quickButtons.set1.numSets}
           </button>
-          <button className="btn">{quickButtons.set2.numSets}</button>
-          <button className="btn">{quickButtons.set3.numSets}</button>
+          <button
+            className="btn"
+            onClick={() => {
+              setTimerInstructions({
+                ...timerInstructions,
+                numSets: quickButtons.set2.numSets,
+              });
+            }}
+          >
+            {quickButtons.set2.numSets}
+          </button>
+          <button
+            className="btn"
+            onClick={() => {
+              setTimerInstructions({
+                ...timerInstructions,
+                numSets: quickButtons.set3.numSets,
+              });
+            }}
+          >
+            {quickButtons.set3.numSets}
+          </button>
         </div>
+
+        {/* TIME ON BUTTONS */}
         <div className="quick-timer-btn">
-          <button className="btn">{quickButtons.on1.duration}</button>
-          <button className="btn">{quickButtons.on2.duration}</button>
-          <button className="btn">{quickButtons.on3.duration}</button>
+          <button
+            className="btn"
+            onClick={() => {
+              setTimerInstructions({
+                ...timerInstructions,
+                timeOn: quickButtons.on1.duration,
+              });
+            }}
+          >
+            {quickButtons.on1.duration}
+          </button>
+          <button
+            className="btn"
+            onClick={() => {
+              setTimerInstructions({
+                ...timerInstructions,
+                timeOn: quickButtons.on2.duration,
+              });
+            }}
+          >
+            {quickButtons.on2.duration}
+          </button>
+          <button
+            className="btn"
+            onClick={() => {
+              setTimerInstructions({
+                ...timerInstructions,
+                timeOn: quickButtons.on3.duration,
+              });
+            }}
+          >
+            {quickButtons.on3.duration}
+          </button>
         </div>
+
+        {/* TIME OFF BUTTONS */}
         <div className="quick-timer-btn">
-          <button className="btn">{quickButtons.off1.duration}</button>
-          <button className="btn">{quickButtons.off2.duration}</button>
-          <button className="btn">{quickButtons.off3.duration}</button>
+          <button
+            className="btn"
+            onClick={() => {
+              setTimerInstructions({
+                ...timerInstructions,
+                timeOff: quickButtons.off1.duration,
+              });
+            }}
+          >
+            {quickButtons.off1.duration}
+          </button>
+          <button
+            className="btn"
+            onClick={() => {
+              setTimerInstructions({
+                ...timerInstructions,
+                timeOff: quickButtons.off2.duration,
+              });
+            }}
+          >
+            {quickButtons.off2.duration}
+          </button>
+          <button
+            className="btn"
+            onClick={() => {
+              setTimerInstructions({
+                ...timerInstructions,
+                timeOff: quickButtons.off3.duration,
+              });
+            }}
+          >
+            {quickButtons.off3.duration}
+          </button>
         </div>
       </div>
+
+      {/* START STOP BUTTONS */}
       <div className="start-stop-wrapper">
         <button
-          className="btn"
+          className={isActive ? "btn pause" : "btn start"}
           onClick={() => {
-            setCounter(3);
+            setCounter(timerInstructions.timeOn);
             setIsActive(!isActive);
           }}
         >
@@ -119,6 +238,9 @@ export const Timer = () => {
         </button>
         <button className="btn">RESET</button>
       </div>
+
+      {/* DEBUG TIMER INSTRUCTIONS */}
+      <div className="text">{JSON.stringify(timerInstructions)}</div>
     </div>
   );
 };
